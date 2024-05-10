91c74c35e969c75419fac4ecc09d72e7cd7a0cc6
yongZhang <kinmer7@gmail.com>
2024-05-10 11:58:42 -0400

    params

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => {
     return (
-        <div>
-            <h1>Adopt Me!</h1>
-            <SearchParams />
-        </div>
+        <BrowserRouter>
+            <header>
+                <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 const container = document.getElementById('root');
